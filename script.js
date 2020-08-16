8d279fc3247a97c8a390bfe1e11a8440a5dff433
Alex-Peeters1969 <67190405+Alex-Peeters1969@users.noreply.github.com>
2020-08-16 13:54:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const age = 16;
+const isFemale = true; 
+const driverStatus = "jan";
+
+
+if (age == 18){
+    console.log ("you have access to enter the bar")
+} 
+else {
+    console.log ("you don't have access to enter the bar")
+}
+if (isFemale == true){
+    console.log ("you are female")
+}
+else {
+    console.log ("you aren't female")
+}
+if (driverStatus == "bob"){
+    console.log ("you can drive")
+}
+else {
+    console.log ("you can't drive")
+}
